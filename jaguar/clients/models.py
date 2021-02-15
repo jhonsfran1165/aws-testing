4c175bf2f4047492d570d08389336f59f85dc7a8
Jhoan Franco <jhonsfran@gmail.com>
2021-02-15 13:03:26 -0500

    fix: some issues starting up the app

--- jaguar/clients/models.py
+++ jaguar/clients/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Clients(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(null=True, blank=True)
+    createdAt = models.DateTimeField(null=True, auto_now_add=True)
+    updatedAt = models.DateTimeField(blank=True)
     isDeleted = models.BooleanField(default=False)
 
     def __str__(self):
